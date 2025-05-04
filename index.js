9a5a9776f99b50119fbea9f93c3bce6d12b13191
Rahul Raj <rahulrajdec65@gmail.com>
2025-05-04 15:35:39 +0530

    Updated frontend url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://auth-frontend-do38.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
